cc355fb159bcfe26e7e1545c0b847192361a27b6
David <davidalonso79@gmail.com>
2020-04-23 19:05:15 +0200

    inicio

---
+++ prestamos_front_admin/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// Peticiones HTTP
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DefaultModule } from './layouts/default/default.module';
+import { FullwidthModule } from './layouts/fullwidth/fullwidth.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthService } from './services/auth.service';
+import { TokenInterceptor } from './shared/interceptors/TokenInterceptor';
+import { ErrorpageComponent } from './modules/errorpage/errorpage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ErrorpageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DefaultModule,
+    FullwidthModule,
+    BrowserAnimationsModule,
+    HttpClientModule
+  ],
+  providers: [
+    AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ prestamos_front_admin/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  client: 'PRESTAMOS',
+  clientPass: '',
+  urlEndPointAuth: 'http://localhost:8099',
+  urlBack: 'http://localhost:8092/api'
+};
