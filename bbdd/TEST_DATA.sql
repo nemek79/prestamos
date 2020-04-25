0bd4b28457f46a3a47f37f8e5a3b9cd63d7c2eee
David <davidalonso79@gmail.com>
2020-04-25 18:10:10 +0200

    data file for testing

---
+++ bbdd/TEST_DATA.sql
@@ -0,0 +1,8 @@
+insert into t_intermediarios values
+(null,'Edwin','Cruz Villalobos',null,'635362205','edwin@edwin.com',20,'MANUAL',current_date(),'MANUAL',current_date());
+
+insert into t_clientes values
+(null,'Anónimo',null,'Cliente desconocido',null,null,'MANUAL',current_date(),'MANUAL',current_date());
+
+insert into t_prestamos values
+(null,'2020-01-01',null,1000,1000,6,20,1,1,1,'MANUAL',current_date(),'MANUAL',current_date());
