e69881afe04aa1012100960ec45431e64fdbd70d
david <davidalonso79@gmail.com>
2020-06-01 09:46:02 +0200

    M0000011 No funciona logout

--- prestamos_front_admin/src/app/shared/header/header.component.ts
+++ prestamos_front_admin/src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authSRV: AuthService,
+    private route: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  public cerrar_sesion(): void {
+
+    this.authSRV.logout();
+    this.route.navigate(['/login']);
+
   }
 
 }
