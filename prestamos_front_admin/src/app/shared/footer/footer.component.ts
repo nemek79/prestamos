4abdb4a6e815e6122ff02798c3cdb7b301930709
david <davidalonso79@gmail.com>
2020-06-03 19:33:00 +0200

    Actualización versiones

--- prestamos_front_admin/src/app/shared/footer/footer.component.ts
+++ prestamos_front_admin/src/app/shared/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  public version = '0.0.1';
+  public version = '0.0.2';
   public year = '2020';
   public url = 'http://vir2al.es';
   public labelUrl = 'Vir2aL';
